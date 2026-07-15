77cd96700bd62d6176034072bd929ed671409775
knde87 <knde87@gmail.com>
2026-07-15 15:26:21 +0900

    Connect Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,16 @@
-﻿const firebaseConfig = {
-  apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
-  authDomain: "REPLACE_WITH_FIREBASE_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_FIREBASE_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_FIREBASE_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyA6nJ_GRR-nKK0-2-6SE2BOmIuLFl8VoS0",
+  authDomain: "goodform1-fd788.firebaseapp.com",
+  projectId: "goodform1-fd788",
+  storageBucket: "goodform1-fd788.firebasestorage.app",
+  messagingSenderId: "847075200575",
+  appId: "1:847075200575:web:2c37717ca9ed9799f7328a"
 };
 
 const socialProviderConfig = {
-  // Firebase Authentication > Sign-in method > OpenID Connect에서 만든 제공자 ID입니다.
-  // Kakao Developers의 Redirect URI에는 아래 주소를 추가해야 합니다.
-  // https://REPLACE_WITH_FIREBASE_AUTH_DOMAIN/__/auth/handler
   kakaoProviderId: "oidc.kakao",
   naverProviderId: "oidc.naver"
 };
 
 window.__FIREBASE_CONFIG__ = firebaseConfig;
 window.__SOCIAL_PROVIDER_CONFIG__ = socialProviderConfig;
-
-
-
-
-
-
-
